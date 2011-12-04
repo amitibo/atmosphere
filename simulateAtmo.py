708d9c453de2954012d0cdd9608e56d65ba15281
amitibo <amitibo@localhost>
2011-12-04 12:24:20 +0200

    Try to implement IPython parallelism. Currently hangs up.

--- simulateAtmo.py
+++ simulateAtmo.py
@@ -34,28 +34,24 @@ http://en.wikipedia.org/wiki/Color
 
 
 from __future__ import division
-from utils import createResultFolder, attrClass
 from enthought.traits.api import HasTraits, Enum, Range, Float, on_trait_change
 from enthought.traits.ui.api import View, Item, VGroup
 from enthought.chaco.api import Plot, ArrayPlotData, VPlotContainer
 from enthought.enable.component_editor import ComponentEditor
-import numpy as np
-from scipy.interpolate import griddata
+import numpy
+import scipy.interpolate
 import argparse
 import math
 import matplotlib.pyplot as plt
 import pickle
 import os
-
+from IPython.parallel import Client, require
 
 #
 # Global parameters
 #
-L_sun_RGB=(255, 236, 224)
-RGB_wavelength = (700e-3, 530e-3, 470e-3)
-h_star = 8000
-eps = np.finfo(float).eps
-SUN_ANGLES = np.linspace(-np.pi/2, np.pi/2, 10)
+SUN_ANGLES = numpy.linspace(-numpy.pi/2, numpy.pi/2, 10)
+
 
 class skyAnalayzer(HasTraits):
     tr_scaling = Range(0.0, 30.0, 0.0, desc='Radiance scaling logarithmic')
@@ -76,7 +72,7 @@ class skyAnalayzer(HasTraits):
         super( skyAnalayzer, self ).__init__()
         
         self.sky_list = sky_list
-        self.tr_sky_max = np.max(self.sky_list[0])
+        self.tr_sky_max = numpy.max(self.sky_list[0])
             
         #
         # Prepare all the plots.
@@ -91,70 +87,76 @@ class skyAnalayzer(HasTraits):
         self.plot_sky = VPlotContainer( plot_img )
 
     def scaleImg(self):
-        sky_list_index = np.argmin(np.abs(SUN_ANGLES - self.tr_sun_angle))
+        sky_list_index = numpy.argmin(numpy.abs(SUN_ANGLES - self.tr_sun_angle))
         
         tmpimg = self.sky_list[sky_list_index]*10**self.tr_scaling
         tmpimg[tmpimg > 255] = 255
-        self.tr_sky_max = np.max(self.sky_list[sky_list_index])
-        return tmpimg.astype(np.uint8)
+        self.tr_sky_max = numpy.max(self.sky_list[sky_list_index])
+        return tmpimg.astype(numpy.uint8)
                    
     @on_trait_change('tr_scaling, tr_sun_angle')
     def _updateImgScale(self):
         self.plotdata.set_data( 'sky_img', self.scaleImg() )
     
 
-def calc_H_Phi_LS(sky_params):
-    """Create the sky matrices: Height matrice, LS (line sight) angles and distances (from the camera) """
-    
-    h, w = np.round(np.array((sky_params.height, sky_params.width)) / sky_params.dxh)
-    X, H = np.meshgrid(np.arange(w)*sky_params.dxh, np.arange(h)[::-1]*sky_params.dxh)
 
-    Phi_LS = np.arctan2(X - sky_params.camera_x, H)
-    Distances = H / np.cos(Phi_LS) + eps
-
-    return H, Phi_LS, Distances
-
-
-def calc_attenuation(H, Distances, sun_angle, Phi_LS, lambda_):
-    """Calc the attenuation of the Sun's radiance per sky voxel.
-The calculation takes into account the path from the top of the sky to the voxel
-and the path from the voxel to the camera."""
-    
-    e_H = np.exp(-H/h_star)
-    alpha = 1.09e-3 * lambda_**-4.05
-    temp = -alpha * ((1 - e_H) / np.cos(Phi_LS) + e_H / np.cos(sun_angle))
-    Phi_scatter = sun_angle + np.pi - Phi_LS
-    attenuation = np.exp(temp) * alpha / h_star * e_H * (1 + np.cos(Phi_scatter)**2)
-    attenuation = attenuation / (Distances + 1)
-
-    print lambda_, np.max(attenuation)
-    return attenuation
-
-
-def cameraProject(Iradiance, Distances, Angles, dist_res, angle_res):
-    """Interpolate a uniform polar grid of a nonuniform polar data"""
-    
-    max_R = np.max(Distances)
-    
-    grid_phi, grid_R = \
-        np.mgrid[-np.pi/2:np.pi/2:np.complex(0, angle_res), 0:max_R:np.complex(0, dist_res)]
-    
-    points = np.vstack((Distances.flatten(), Angles.flatten())).T
-    polar_attenuation = griddata(points, Iradiance.flatten(), (grid_R, grid_phi), method='cubic', fill_value=0)
-    polar_attenuation[polar_attenuation<0] = 0
-    
-    jac = np.linspace(0, max_R, dist_res)
-    camera_projection = np.sum(polar_attenuation * jac, axis = 1)
-    
-    return camera_projection
-
-
-def calcCamIR(sky_params, sun_angle):
+@require(numpy, scipy.interpolate)
+def calcCamIR(params):
     """Calculate the Iradiance at the camera"""
     
+    L_sun_RGB=(255, 236, 224)
+    RGB_wavelength = (700e-3, 530e-3, 470e-3)
+
+    def calc_H_Phi_LS(sky_params):
+        """Create the sky matrices: Height matrice, LS (line sight) angles and distances (from the camera) """
+        
+        h, w = numpy.round(numpy.array((sky_params['height'], sky_params['width'])) / sky_params['dxh'])
+        X, H = numpy.meshgrid(numpy.arange(w)*sky_params['dxh'], numpy.arange(h)[::-1]*sky_params['dxh'])
+    
+        Phi_LS = numpy.arctan2(X - sky_params['camera_x'], H)
+        Distances = H / numpy.cos(Phi_LS) + numpy.finfo(float).eps
+    
+        return H, Phi_LS, Distances
+    
+    
+    def calc_attenuation(H, Distances, sun_angle, Phi_LS, lambda_):
+        """Calc the attenuation of the Sun's radiance per sky voxel.
+    The calculation takes into account the path from the top of the sky to the voxel
+    and the path from the voxel to the camera."""
+        
+        h_star = 8000
+        e_H = numpy.exp(-H/h_star)
+        alpha = 1.09e-3 * lambda_**-4.05
+        temp = -alpha * ((1 - e_H) / numpy.cos(Phi_LS) + e_H / numpy.cos(sun_angle))
+        Phi_scatter = sun_angle + numpy.pi - Phi_LS
+        attenuation = numpy.exp(temp) * alpha / h_star * e_H * (1 + numpy.cos(Phi_scatter)**2)
+        attenuation = attenuation / (Distances + 1)
+    
+        print lambda_, numpy.max(attenuation)
+        return attenuation
+    
+    
+    def cameraProject(Iradiance, Distances, Angles, dist_res, angle_res, interp_method):
+        """Interpolate a uniform polar grid of a nonuniform polar data"""
+        
+        max_R = numpy.max(Distances)
+        
+        grid_phi, grid_R = \
+            numpy.mgrid[-numpy.pi/2:numpy.pi/2:numpy.complex(0, angle_res), 0:max_R:numpy.complex(0, dist_res)]
+        
+        points = numpy.vstack((Distances.flatten(), Angles.flatten())).T
+        polar_attenuation = scipy.interpolate.griddata(points, Iradiance.flatten(), (grid_R, grid_phi), method=interp_method, fill_value=0)
+        polar_attenuation[polar_attenuation<0] = 0
+        
+        jac = numpy.linspace(0, max_R, dist_res)
+        camera_projection = numpy.sum(polar_attenuation * jac, axis = 1)
+        
+        return camera_projection
+
+    sky_params, sun_angle = params
     H, Phi_LS, Distances = calc_H_Phi_LS(sky_params)
 
-    cam_radiance = np.zeros((sky_params.angle_res, 3))
+    cam_radiance = numpy.zeros((sky_params['angle_res'], 3))
    
     #
     # Caluclate the iradiance separately for each color channel.
@@ -166,14 +168,15 @@ def calcCamIR(sky_params, sun_angle):
                                     Iradiance,
                                     Distances,
                                     Phi_LS,
-                                    sky_params.dist_res,
-                                    sky_params.angle_res
+                                    sky_params['dist_res'],
+                                    sky_params['angle_res'],
+                                    sky_params['interp_method']
                                     )
 
     #
     # Tile the 2D camera to 3D camera.
     #
-    cam_radiance = np.tile(cam_radiance.reshape(1, sky_params.angle_res, 3), (sky_params.angle_res, 1, 1))  
+    cam_radiance = numpy.tile(cam_radiance.reshape(1, sky_params['angle_res'], 3), (sky_params['angle_res'], 1, 1))  
     
     return cam_radiance
 
@@ -197,21 +200,27 @@ def main():
         #
         # Set the params of the run
         #
-        sky_params = attrClass(
-                    width=1e5,
-                    height=1e4,
-                    dxh=50,
-                    camera_x=1e5/2,
-                    angle_res=360,
-                    dist_res=100
-                    )
+        sky_params = {
+                    "width": 1e5,
+                    "height": 1e4,
+                    "dxh": 50,
+                    "camera_x": 1e5/2,
+                    "angle_res": 360,
+                    "dist_res": 100,
+                    "interp_method": 'cubic'
+        }
     
+        #
+        # Handle the parallelism
+        #
+        rc = Client()
+        lview = rc.load_balanced_view()
+        lview.block = True
+        
         #
         # Run the simulation
         #
-        cam_radiances = []
-        for sun_angle in SUN_ANGLES:
-            cam_radiances.append(calcCamIR(sky_params, sun_angle))
+        cam_radiances = lview.map(calcCamIR, [(sky_params, sun_angle) for sun_angle in SUN_ANGLES])
         
         file_path = os.path.join(results_folder, 'blue_sky.pkl')
         with open(file_path, 'wb') as f:
