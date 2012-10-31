09c627bb1963b36d8e8eb5e6d70f8cbf06626d47
Amit Aides <amitibo@tx.technion.ac.il>
2012-10-31 17:08:14 +0200

    Added missing files to previous commit

---
+++ analyzeAtmoGeneral3D.py
@@ -0,0 +1,325 @@
+"""
+Reconstruct a general distribution of aerosols in the atmosphere.
+"""
+from __future__ import division
+import numpy as np
+from atmo_utils import calcHG, L_SUN_RGB, RGB_WAVELENGTH
+from camera import Camera
+import pickle
+import ipopt
+import logging
+import amitibo
+import os
+
+from mpi4py import MPI
+
+comm = MPI.COMM_WORLD
+
+mpi_size = MPI.COMM_WORLD.Get_size()
+mpi_rank = MPI.COMM_WORLD.Get_rank()
+
+OBJTAG = 1
+GRADTAG = 2
+DIETAG = 3
+
+MAX_ITERATIONS = 1
+
+#
+# Global settings
+#
+atmosphere_params = amitibo.attrClass(
+    cartesian_grids=(
+        slice(0, 400, 4), # Y
+        slice(0, 400, 4), # X
+        slice(0, 80, 1)   # H
+        ),
+    earth_radius=4000,
+    L_SUN_RGB=L_SUN_RGB,
+    RGB_WAVELENGTH=RGB_WAVELENGTH,
+    air_typical_h=8,
+    aerosols_typical_h=1.2
+)
+
+camera_params = amitibo.attrClass(
+    radius_res=20,
+    phi_res=40,
+    theta_res=40,
+    focal_ratio=0.15,
+    image_res=128,
+    theta_compensation=False
+)
+
+CAMERA_CENTERS = [(i, 200, 0.2) for i in np.linspace(100, 300, mpi_size-1)]
+SUN_ANGLE = np.pi/4
+
+profile = False
+
+
+class radiance(object):
+    def __init__(self):
+
+        #
+        # Create the sky
+        #
+        self.Y, self.X, self.H = np.mgrid[atmosphere_params.cartesian_grids]
+        width = atmosphere_params.cartesian_grids[0].stop
+        height = atmosphere_params.cartesian_grids[2].stop
+        h = np.sqrt((self.X-width/2)**2 + (self.Y-width/2)**2 + (atmosphere_params.earth_radius+self.H)**2) - atmosphere_params.earth_radius
+    
+        #
+        # Create the distributions of air & aerosols
+        #
+        self.ATMO_aerosols = np.exp(-h/atmosphere_params.aerosols_typical_h)
+        self.ATMO_air = np.exp(-h/atmosphere_params.air_typical_h)
+
+        #
+        # Create the first images
+        #
+        for i in range(1, mpi_size):
+            comm.send([self.ATMO_aerosols, self.ATMO_air], dest=i, tag=OBJTAG)
+
+        sts = MPI.Status()
+        self.Images = range(mpi_size-1)
+        
+        for i in range(1, mpi_size):
+            img = comm.recv(source=MPI.ANY_SOURCE, status=sts)
+            src = sts.Get_source()
+            
+            self.Images[src-1] = img
+
+        self.obj_value = []
+
+    def getX0(self):
+        #
+        # Create the initial aerosols distribution
+        #
+        ATMO_aerosols = np.ones(self.H.shape)
+        return ATMO_aerosols.reshape((-1, 1))
+    
+    def objective(self, x):
+        """Calculate the objective"""
+
+        print 'objective calculation.'
+        
+        for i in range(1, mpi_size):
+            comm.send([x, self.ATMO_air], dest=i, tag=OBJTAG)
+
+        print 'sent messages'
+        
+        sts = MPI.Status()
+        images = range(mpi_size-1)
+        
+        for i in range(1, mpi_size):
+            img = comm.recv(source=MPI.ANY_SOURCE, status=sts)
+            src = sts.Get_source()
+            
+            images[src-1] = img
+
+        print 'recived messages'
+        
+        obj = 0
+        for ref_img, img in zip(self.Images, images):
+            o = [np.dot(
+                (ref_img[i] - img[i]).T,
+                (ref_img[i] - img[i])
+                ) for i in range(3)]
+            obj += np.sum(o)
+            
+        print 'calculating objective'
+        
+        return obj
+    
+    def gradient(self, x):
+        """The callback for calculating the gradient"""
+
+        for i in range(1, mpi_size):
+            comm.send([x, self.ATMO_air], dest=i, tag=OBJTAG)
+
+        sts = MPI.Status()
+        images = range(mpi_size-1)
+        
+        for i in range(1, mpi_size):
+            img = comm.recv(source=MPI.ANY_SOURCE, status=sts)
+            src = sts.Get_source()
+            
+            images[src-1] = img
+            
+        for i in range(1, mpi_size):
+            comm.send([x, self.ATMO_air], dest=i, tag=GRADTAG)
+
+        grads = range(mpi_size-1)
+        
+        for i in range(1, mpi_size):
+            gimg = comm.recv(source=MPI.ANY_SOURCE, status=sts)
+            src = sts.Get_source()
+            
+            grads[src-1] = gimg
+
+        grad = None
+        for ref_img, img, gimg in zip(self.Images, images, grads):
+
+            temp = [-2*(gimg[i]*(ref_img[camera_index][i] - img[i]).reshape((-1, 1))) for i in range(3)]
+            
+            g = np.sum(np.hstack(temp), axis=1)
+
+            if grad == None:
+                grad = g
+            else:
+                grad += g
+            
+        return grad
+
+    def intermediate(
+            self,
+            alg_mod,
+            iter_count,
+            obj_value,
+            inf_pr,
+            inf_du,
+            mu,
+            d_norm,
+            regularization_size,
+            alpha_du,
+            alpha_pr,
+            ls_trials
+            ):
+
+        self.obj_value.append(obj_value)
+        return True
+    
+    def constraints(self, x):
+        #
+        # The callback for calculating the constraints
+        #
+        return None
+    
+    
+    def jacobian(self, x):
+        #
+        # The callback for calculating the Jacobian
+        #
+        return None
+    
+
+def master():
+    #
+    # Define the problem
+    #
+    sky = radiance()
+
+    x0 = sky.getX0()
+
+    lb = np.zeros(x0.shape)
+    ub = np.ones(x0.shape)
+    
+    cl = []
+    cu = []
+
+    import logging
+    logging.basicConfig(filename='run.log',level=logging.DEBUG)
+    ipopt.setLoggingLevel(logging.DEBUG)
+    
+    nlp = ipopt.problem(
+                n=len(x0),
+                m=len(cl),
+                problem_obj=sky,
+                lb=lb,
+                ub=ub,
+                cl=cl,
+                cu=cu
+                )
+
+    #
+    # Set solver options
+    #
+    #nlp.addOption('derivative_test', 'first-order')
+    nlp.addOption('hessian_approximation', 'limited-memory')
+    nlp.addOption('mu_strategy', 'adaptive')
+    nlp.addOption('tol', 1e-7)
+    nlp.addOption('max_iter', MAX_ITERATIONS)
+
+    #
+    # Solve the problem
+    #
+    x, info = nlp.solve(x0)
+
+    #
+    # Kill all slaves
+    #
+    for i in range(1, mpi_size):
+        comm.send(0, dest=i, tag=DIETAG)
+    
+    
+def slave(particle_params):
+    #
+    # Instatiate the camera slave
+    #
+    cam = Camera(
+        SUN_ANGLE,
+        atmosphere_params=atmosphere_params,
+        camera_params=camera_params,
+        camera_position=CAMERA_CENTERS[mpi_rank-1]
+    )
+    
+    sts = MPI.Status()
+
+    while 1:
+        data = comm.recv(source=0, tag=MPI.ANY_TAG, status=sts)
+
+        tag = sts.Get_tag()
+        if tag == DIETAG:
+            break
+
+        A_air = data[0]
+        A_aerosols = data[1]
+        
+        if tag == OBJTAG:
+            img = cam.calcImage(
+                A_air=A_air,
+                A_aerosols=A_aerosols,
+                particle_params=particle_params
+            )
+            
+            comm.send(img, dest=0)
+        elif tag == GRADTAG:
+            gimg = cam.calcImageGradient(
+                A_air=A_air,
+                A_aerosols=A_aerosols,
+                particle_params=particle_params
+            )
+
+            comm.send(gimg, dest=0)
+        else:
+            raise Exception('Unkown tag')
+
+
+def main():
+    #
+    # Load the MISR database.
+    #
+    with open('misr.pkl', 'rb') as f:
+        misr = pickle.load(f)
+
+    #
+    # Set aerosol parameters
+    #
+    particles_list = misr.keys()
+    particle = misr[particles_list[0]]
+    particle_params = amitibo.attrClass(
+        k_RGB=np.array(particle['k']) / np.max(np.array(particle['k'])),#* 10**-12,
+        w_RGB=particle['w'],
+        g_RGB=(particle['g']),
+        visibility=10
+        )
+    
+    if mpi_rank == 0:
+        #
+        # Set up the solver server.
+        #
+        master()
+    else:
+        slave(particle_params)
+
+
+if __name__ == '__main__':
+    main()
--- simulateAtmoGeneral3D.py
+++ simulateAtmoGeneral3D.py
@@ -4,7 +4,7 @@ Simulate the scattering of the sky where the aerosols have a general distributio
 
 from __future__ import division
 import numpy as np
-from atmo_utils import calcHG, L_SUN_RGB, RGB_WAVELENGTH, spdiag, viz2D, viz3D
+from atmo_utils import calcHG, L_SUN_RGB, RGB_WAVELENGTH
 import atmo_utils
 from camera import Camera
 import amitibo
@@ -21,9 +21,9 @@ import sys
 #
 atmosphere_params = amitibo.attrClass(
     cartesian_grids=(
-        slice(0, 400, 4),
-        slice(0, 400, 4),
-        slice(0, 80, 1)
+        slice(0, 400, 4), # Y
+        slice(0, 400, 4), # X
+        slice(0, 80, 1)   # H
         ),
     earth_radius=4000,
     L_SUN_RGB=L_SUN_RGB,
@@ -33,212 +33,24 @@ atmosphere_params = amitibo.attrClass(
 )
 
 camera_params = amitibo.attrClass(
-    radius_res=40,
-    phi_res=80,
-    theta_res=80,
+    radius_res=20,
+    phi_res=40,
+    theta_res=40,
     focal_ratio=0.15,
-    image_res=512,
+    image_res=128,
     theta_compensation=False
 )
 
 profile = False
 
 
-def calcRadianceGradientHelper(
-    ATMO_aerosols_,
-    ATMO_air_,
-    Y,
-    X,
-    H,
-    aerosol_params,
-    sky_params,
-    camera_center
-    ):
-    """
-    Helper function that does the actual calculation of the radiance gradient.
-    """
-    
-    ATMO_aerosols_ = ATMO_aerosols_.reshape((-1, 1))
-    ATMO_air_ = ATMO_air_.reshape((-1, 1))
-    
-    H_pol, R, PHI, THETA = atmo_utils.sphericalTransformMatrix(
-        Y,
-        X,
-        H,
-        camera_center,
-        radius_res=sky_params['radius_res'],
-        phi_res=sky_params['phi_res'],
-        theta_res=sky_params['theta_res']
-        )
-    
-    #
-    # Calculate the distance matrices and scattering angle
-    #
-    H_distances, scatter_angle = \
-        calcOpticalDistancesMatrix(
-            Y,
-            X,
-            H,
-            sky_params['sun_angle'],
-            H_pol,
-            R,
-            PHI,
-            THETA
-            )
 
-    scatter_angle.shape = (-1, 1)
-    
-    H_int = atmo_utils.integralTransformMatrix((R, PHI, THETA), axis=0)
-    H_camera = atmo_utils.cameraTransformMatrix(PHI[0, :, :], THETA[0, :, :], focal_ratio=sky_params['focal_ratio'], image_res=sky_params['image_res'])
-
-    #vizTransforms2(
-        #H_pol,
-        #H_int,
-        #H_camera,
-        #ATMO_air_,
-        #Y,
-        #X,
-        #H,
-        #R,
-        #PHI,
-        #THETA
-    #)
-    
-    mu = numpy.cos(scatter_angle)
-
-    #
-    # Calculate scattering for each channel (in case of the railey scattering)
-    #
-    gimg = []
-    for L_sun, lambda_, k, w, g in zip(
-            sky_params["L_SUN_RGB"],
-            sky_params["RGB_WAVELENGTH"],
-            aerosol_params["k_RGB"],
-            aerosol_params["w_RGB"],
-            aerosol_params["g_RGB"]
-            ):
-        #
-        # Calculate scattering and extiniction for aerosols
-        #
-        extinction_aerosols = k / aerosol_params["visibility"]
-        scatter_aerosols_partial = w * extinction_aerosols * calcHG(mu, g)
-        scatter_aerosols = spdiag(scatter_aerosols_partial * (H_pol * ATMO_aerosols_))
-        scatter_aerosols_grad = H_pol.T * spdiag(scatter_aerosols_partial)
-        exp_aerosols = spdiag(numpy.exp(-extinction_aerosols * H_distances * ATMO_aerosols_))
-        exp_aerosols_grad = -extinction_aerosols * H_distances.T * exp_aerosols
-
-        #
-        # Calculate scattering and extiniction for air (wave length dependent)
-        #
-        extinction_air = 1.09e-3 * lambda_**-4.05
-        scatter_air = spdiag(extinction_air * (1 + mu**2) * 3 / (16*numpy.pi) * (H_pol * ATMO_air_))
-        exp_air = spdiag(numpy.exp(-extinction_air * H_distances * ATMO_air_))
-
-        #
-        # Calculate the gradient of the radiance
-        #
-        temp1 = scatter_aerosols_grad * exp_aerosols + exp_aerosols_grad * scatter_aerosols
-        temp2 = exp_aerosols_grad * scatter_air
-        radiance_gradient = (temp1 + temp2) * exp_air
-
-        #
-        # Calculate projection on camera
-        #
-        gimg.append(L_sun * radiance_gradient * H_int.T * H_camera.T)
-        
-    return gimg
-
-
-def calcRadianceGradient(ATMO_aerosols, ATMO_air, aerosol_params, sky_params):
-    """
-    Calculate the gradient of the radiance at some atmospheric distribution.
-    """
-
-    #
-    # Rowwise representation of the atmosphere 
-    #
-    ATMO_aerosols_ = ATMO_aerosols.reshape((-1, 1))
-    ATMO_air_ = ATMO_air.reshape((-1, 1))
-
-    #
-    # Create the sky
-    #
-    X, H = numpy.meshgrid(
-        numpy.arange(0, sky_params['width'], sky_params['dx']),
-        numpy.arange(0, sky_params['height'], sky_params['dh'])[::-1]
-        )
-
-    img = calcRadianceGradientHelper(
-        ATMO_aerosols_,
-        ATMO_air_,
-        X,
-        H,
-        aerosol_params,
-        sky_params,
-        sky_params['camera_center']
-        )
-
-    return img
-
-
-def main_parallel(aerosol_params, sky_params, results_path=''):
-    """Run the calculation in parallel on a space of parameters"""
-
-    sun_angle_range = numpy.linspace(0, numpy.pi/2, 32)
-    
-    job_server = amitibo.start_jobServer()
-    jobs = []
-
-    for sun_angle in sun_angle_range:
-        sky_params['sun_angle'] = sun_angle
-        jobs.append(
-            job_server.submit(
-                calcRadiance,
-                args=(aerosol_params, sky_params, results_path),
-                depfuncs=amitibo.depfuncs(globals()),
-                modules=(
-                    'numpy',
-                    'math',
-                    'atmo_utils',
-                    'amitibo'
-                )
-            )
-        )
-
-    images = []
-    max_values = []
-    for job in jobs:
-        img = job()
-        max_values.append(numpy.max(img))
-        images.append(img)
-
-    max_value = numpy.max(numpy.array(max_values))
-    figures = []
-    for img, sun_angle in zip(images, sun_angle_range):
-        #
-        # Account for gamma correction
-        #
-        img /= max_value
-        img = img**0.45
-    
-        #
-        # Plot results
-        #
-        fig = draw_image(img, sky_params['focal_ratio'], sun_angle)
-        amitibo.saveFigures(results_path, (fig, ), bbox_inches='tight')
-        figures.append(fig)
-
-    pdf = amitibo.figuresPdf(os.path.join(results_path, 'report.pdf'))
-    pdf.saveFigures(figures)
-
-
-def parallel(particle_params, results_path):
+def parallel(particle_params):
     
     from mpi4py import MPI
     
     comm = MPI.COMM_WORLD
 
-    print atmosphere_params.cartesian_grids
     Y, X, H = np.mgrid[atmosphere_params.cartesian_grids]
     width = atmosphere_params.cartesian_grids[0].stop
     height = atmosphere_params.cartesian_grids[2].stop
@@ -263,8 +75,6 @@ def parallel(particle_params, results_path):
     #
     img = cam.calcImage(A_air=ATMO_air, A_aerosols=ATMO_aerosols, particle_params=particle_params)
         
-    print 'finished processing %d on %s with %g and %g' % (comm.rank, socket.gethostname(), sun_angles[comm.rank], img.max())
-    
     result = comm.gather(img, root=0)
     if comm.rank == 0:
         results_path = amitibo.createResultFolder(params=[atmosphere_params, particle_params, camera_params])
@@ -273,7 +83,7 @@ def parallel(particle_params, results_path):
             sio.savemat(os.path.join(results_path, 'img%d.mat' % i), {'img':img}, do_compression=True)
     
     
-def serial(particle_params, results_path):
+def serial(particle_params):
     
     #
     # Create some aerosols distribution
@@ -302,6 +112,7 @@ def serial(particle_params, results_path):
         #
         img = cam.calcImage(A_air=ATMO_air, A_aerosols=ATMO_aerosols, particle_params=particle_params)
         
+        results_path = amitibo.createResultFolder(params=[atmosphere_params, particle_params, camera_params])
         sio.savemat(os.path.join(results_path, 'img%d.mat' % i), {'img':img}, do_compression=True)
         
 
@@ -324,15 +135,9 @@ if __name__ == '__main__':
         visibility=10
         )
 
-    #
-    # Create afolder for results
-    #
-    #results_path = amitibo.createResultFolder(params=[atmosphere_params, particle_params, camera_params])
-    results_path = None
-    
     if profile:
         import cProfile    
-        cmd = "serial(SKY_PARAMS, aerosol_params, results_path)"
+        cmd = "serial(particle_params)"
         cProfile.runctx(cmd, globals(), locals(), filename="atmosphere_camera.profile")
     else:
-        parallel(particle_params, results_path)
+        parallel(particle_params)
