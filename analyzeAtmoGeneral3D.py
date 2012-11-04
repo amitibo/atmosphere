fe6bde3bd4f752f5b7ab42920e40ce8eb2e108f8
Amit Aides <amitibo@tx.technion.ac.il>
2012-11-04 13:54:24 +0200

    Refactoring of the optimization code. Added storing of initial and final images

--- analyzeAtmoGeneral3D.py
+++ analyzeAtmoGeneral3D.py
@@ -32,9 +32,9 @@ MAX_ITERATIONS = 1000
 #
 atmosphere_params = amitibo.attrClass(
     cartesian_grids=(
-        slice(0, 400, 4), # Y
-        slice(0, 400, 4), # X
-        slice(0, 80, 4)   # H
+        slice(0, 400, 8), # Y
+        slice(0, 400, 8), # X
+        slice(0, 80, 8)   # H
         ),
     earth_radius=4000,
     L_SUN_RGB=L_SUN_RGB,
@@ -44,7 +44,7 @@ atmosphere_params = amitibo.attrClass(
 )
 
 camera_params = amitibo.attrClass(
-    radius_res=40,
+    radius_res=20,
     phi_res=40,
     theta_res=40,
     focal_ratio=0.15,
@@ -52,44 +52,37 @@ camera_params = amitibo.attrClass(
     theta_compensation=False
 )
 
+#CAMERA_CENTERS = [(i, 200, 0.2) for i in np.linspace(100, 300, mpi_size-1)]
 CAMERA_CENTERS = [(i, j, 0.2) for i, j in itertools.product(np.linspace(100, 300, 6), np.linspace(100, 300, 6))]
 SUN_ANGLE = np.pi/4
 
 profile = False
 
 
-class Radiance(object):
-    def __init__(self):
+class RadianceProblem(ipopt.problem):
+    def __init__(self, A_aerosols, A_air, results_path):
 
-        #
-        # Create the sky
-        #
-        self.Y, self.X, self.H = np.mgrid[atmosphere_params.cartesian_grids]
-        width = atmosphere_params.cartesian_grids[0].stop
-        height = atmosphere_params.cartesian_grids[2].stop
-        h = np.sqrt((self.X-width/2)**2 + (self.Y-width/2)**2 + (atmosphere_params.earth_radius+self.H)**2) - atmosphere_params.earth_radius
-    
-        #
-        # Create the distributions of air & aerosols
-        #
-        self.ATMO_aerosols = np.exp(-h/atmosphere_params.aerosols_typical_h)
-        self.ATMO_air = np.exp(-h/atmosphere_params.air_typical_h)
-
+        lb = np.zeros(A_aerosols.size)
+        ub = np.ones(A_aerosols.size)
+            
+        super(RadianceProblem, self).__init__(
+                    n=A_aerosols.size,
+                    m=0,
+                    problem_obj=self,
+                    lb=lb,
+                    ub=ub,
+                    cl=[],
+                    cu=[]
+                    )
+        
         #
         # Send the real atmospheric distribution to all childs so as to create the measurement.
         #
         for i in range(1, mpi_size):
-            comm.send([self.ATMO_aerosols, self.ATMO_air], dest=i, tag=IMGTAG)
+            comm.send([A_aerosols, A_air, results_path], dest=i, tag=IMGTAG)
 
         self.obj_value = []
 
-    def getX0(self):
-        #
-        # Create the initial aerosols distribution
-        #
-        ATMO_aerosols = np.ones(self.H.shape)
-        return ATMO_aerosols.reshape((-1, 1))
-    
     def objective(self, x):
         """Calculate the objective"""
 
@@ -159,59 +152,60 @@ class Radiance(object):
 def master(particle_params):
     #import rpdb2; rpdb2.start_embedded_debugger('pep')
     
-    #
-    # Define the problem
-    #
-    sky = Radiance()
+    results_path = amitibo.createResultFolder(params=[atmosphere_params, particle_params, camera_params])
 
-    x0 = sky.getX0()
+    #
+    # Create the sky
+    #
+    Y, X, H = np.mgrid[atmosphere_params.cartesian_grids]
+    width = atmosphere_params.cartesian_grids[0].stop
+    height = atmosphere_params.cartesian_grids[2].stop
+    h = np.sqrt((X-width/2)**2 + (Y-width/2)**2 + (atmosphere_params.earth_radius+H)**2) - atmosphere_params.earth_radius
 
-    lb = np.zeros(x0.shape)
-    ub = np.ones(x0.shape)
-    
-    cl = []
-    cu = []
+    #
+    # Create the distributions of air & aerosols
+    #
+    A_aerosols = np.exp(-h/atmosphere_params.aerosols_typical_h)
+    A_air = np.exp(-h/atmosphere_params.air_typical_h)
 
     import logging
     logging.basicConfig(filename='run.log',level=logging.DEBUG)
     ipopt.setLoggingLevel(logging.DEBUG)
     
-    nlp = ipopt.problem(
-                n=len(x0),
-                m=len(cl),
-                problem_obj=sky,
-                lb=lb,
-                ub=ub,
-                cl=cl,
-                cu=cu
-                )
+    #
+    # Define the problem
+    #
+    problem = RadianceProblem(
+        A_aerosols=A_aerosols,
+        A_air=A_air,
+        results_path=results_path
+    )
 
     #
     # Set solver options
     #
-    #nlp.addOption('derivative_test', 'first-order')
-    nlp.addOption('hessian_approximation', 'limited-memory')
-    nlp.addOption('mu_strategy', 'adaptive')
-    nlp.addOption('tol', 1e-7)
-    nlp.addOption('max_iter', MAX_ITERATIONS)
+    #problem.addOption('derivative_test', 'first-order')
+    problem.addOption('hessian_approximation', 'limited-memory')
+    problem.addOption('mu_strategy', 'adaptive')
+    problem.addOption('tol', 1e-9)
+    problem.addOption('max_iter', MAX_ITERATIONS)
 
     #
     # Solve the problem
     #
-    x, info = nlp.solve(x0)
+    x0 = np.zeros_like(A_aerosols).reshape((-1, 1))
+    x, info = problem.solve(x0)
 
     #
     # Kill all slaves
     #
     for i in range(1, mpi_size):
-        comm.Send(0, dest=i, tag=DIETAG)
+        comm.Send(x, dest=i, tag=DIETAG)
     
     #
     # Store the result
     #
-    Y, X, H = np.mgrid[atmosphere_params.cartesian_grids]
-    results_path = amitibo.createResultFolder(params=[atmosphere_params, particle_params, camera_params])
-    sio.savemat(os.path.join(results_path, 'radiance.mat'), {'radiance': x.reshape(X.shape)}, do_compression=True)
+    sio.savemat(os.path.join(results_path, 'radiance.mat'), {'radiance': x.reshape(A_aerosols.shape)}, do_compression=True)
     
     
 def slave(particle_params):
@@ -238,7 +232,8 @@ def slave(particle_params):
 
     A_air = data[0]
     A_aerosols = data[1]
-
+    results_path = data[2]
+    
     cam.setA_air(A_air)
     
     ref_img = cam.calcImage(
@@ -246,6 +241,15 @@ def slave(particle_params):
         particle_params=particle_params
     )
 
+    sio.savemat(
+        os.path.join(results_path, 'ref_img%d.mat' % mpi_rank),
+        {'img': ref_img},
+        do_compression=True
+    )
+
+    #
+    # Loop the messages of the master
+    #
     while 1:
         comm.Recv(A_aerosols, source=0, tag=MPI.ANY_TAG, status=sts)
 
@@ -282,6 +286,25 @@ def slave(particle_params):
             comm.Send(grad, dest=0)
         else:
             raise Exception('Unexpected tag %d' % tag)
+
+        ref_img = cam.calcImage(
+            A_aerosols=A_aerosols,
+            particle_params=particle_params
+        )
+
+    #
+    # Save the image the relates to the calculated aerosol distribution
+    #
+    final_img = cam.calcImage(
+        A_aerosols=A_aerosols,
+        particle_params=particle_params
+    )
+
+    sio.savemat(
+        os.path.join(results_path, 'final_img%d.mat' % mpi_rank),
+        {'img': final_img},
+        do_compression=True
+    )
 
 
 def main():
