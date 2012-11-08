Bug fixes and cleanup in the image and gradient calculations. Some improvment (the objective graph shows improvement over iterations). Though still does not give good results overall.

analyzeAtmoGeneral3D.py
@@ -8,7 +8,6 @@ import scipy.io as sio
 from camera import Camera
 import pickle
 import ipopt
-import logging
 import amitibo
 import itertools
 import os
@@ -34,16 +33,16 @@ OBJTAG = 2
 GRADTAG = 3
 DIETAG = 4
 
-MAX_ITERATIONS = 10
+MAX_ITERATIONS = 200
 
 #
 # Global settings
 #
 atmosphere_params = amitibo.attrClass(
     cartesian_grids=(
-        slice(0, 400, 40), # Y
-        slice(0, 400, 40), # X
-        slice(0, 80, 20)   # H
+        slice(0, 400, 8), # Y
+        slice(0, 400, 8), # X
+        slice(0, 80, 8)   # H
         ),
     earth_radius=4000,
     L_SUN_RGB=L_SUN_RGB,
@@ -99,8 +98,7 @@ class RadianceProblem(ipopt.problem):
             comm.Send(x, dest=i, tag=OBJTAG)
 
         sts = MPI.Status()
-        images = range(mpi_size-1)
-        
+
         obj = 0
         temp = np.empty(1)
         for i in range(1, mpi_size):
@@ -116,7 +114,6 @@ class RadianceProblem(ipopt.problem):
             comm.Send(x, dest=i, tag=GRADTAG)
 
         sts = MPI.Status()
-        images = range(mpi_size-1)
 
         temp = np.empty((x.size, 1))
         grad = np.zeros_like(temp)
@@ -191,6 +188,8 @@ def master(particle_params):
     #
     #problem.addOption('derivative_test', 'first-order')
     #problem.addOption('derivative_test_print_all', 'yes')
+    #problem.addOption('derivative_test_tol', 5e-3)
+    #problem.addOption('derivative_test_perturbation', 1e-8)
     problem.addOption('hessian_approximation', 'limited-memory')
     problem.addOption('mu_strategy', 'adaptive')
     problem.addOption('tol', 1e-9)

camera.py
@@ -32,31 +32,24 @@ def calcOpticalDistancesMatrix(Y, X, Z, sun_angle, H_pol, R, PHI, THETA):
     temp1 = H_pol * Hrot_backward * Hint1 * Hrot_forward
     temp2 = Hint2 * H_pol
 
-    scatter_angle = calcScatterAngle(R, PHI, THETA, rotation)
-    
     #vizTransforms(Y, X, Z, H_pol, Hrot_forward, Hrot_backward, Hint1, Hint2, R, Y_rot, scatter_angle)
     
-    return temp2 + temp1, scatter_angle
+    return temp2 + temp1
 
 
-def calcScatterAngle(R, PHI, THETA, rotation):
+def calcScatterAngle(Y, X, Z, camera_position, sun_angle):
     """
     Calclculate the scattering angle at each voxel.
     """
 
-    X_ = R * np.sin(THETA) * np.cos(PHI)
-    Y_ = R * np.sin(THETA) * np.sin(PHI)
-    Z_ = R * np.cos(THETA)
+    Y_ = Y-camera_position[0]
+    X_ = X-camera_position[1]
+    Z_ = Z-camera_position[2]
+    R = np.sqrt(Y_**2 + X_**2 + Z_**2)
 
-    XYZ_dst = np.vstack((X_.ravel(), Y_.ravel(), Z_.ravel(), np.ones(R.size)))
-    XYZ_src_ = np.dot(rotation, XYZ_dst)
-
-    Z_rotated = XYZ_src_[2, :]
-    R_rotated = np.sqrt(np.sum(XYZ_src_[:3, :]**2, axis=0))
-
-    angle = np.arccos(Z_rotated/(R_rotated+amitibo.eps(R_rotated)))
-
-    return angle
+    mu = (np.sin(sun_angle) * Y_ + np.cos(sun_angle) * Z_)/ (R + amitibo.eps(R))
+    
+    return mu
 
 
 class Camera(object):
@@ -89,7 +82,7 @@ class Camera(object):
         #
         # Calculate the distance matrices and scattering angle
         #
-        H_distances, scatter_angle = \
+        H_distances = \
             calcOpticalDistancesMatrix(
                 Y,
                 X,
@@ -111,12 +104,17 @@ class Camera(object):
         )
 
         #
+        # Calculate the mu
         #
+        mu = calcScatterAngle(Y, X, H, camera_position, sun_angle)
+        
+        #
+        # Store the matrices
         #
         self.H_distances = H_distances
         self.H_pol = H_pol
         self.H_sensor = H_camera * H_int
-        self.mu = np.cos(scatter_angle).reshape((-1, 1))
+        self.mu = mu.reshape((-1, 1))
         self.camera_params = camera_params
         self.atmosphere_params = atmosphere_params
 
@@ -133,9 +131,8 @@ class Camera(object):
         #
         # Precalcuate the air scattering and attenuation
         #
-        scatter_air_pre = (1 + self.mu**2) * 3 / (16*np.pi) * (self.H_pol * self.A_air_)
+        scatter_air_pre = 3 / (16*np.pi) * self.H_pol * ((1 + self.mu**2) * self.A_air_)
         exp_air_pre = self.H_distances * self.A_air_
-        scatter_aerosols_pre = self.H_pol * A_aerosols_
         exp_aerosols_pre = self.H_distances * A_aerosols_
 
         img = []
@@ -150,7 +147,7 @@ class Camera(object):
             # Calculate scattering and extiniction for aerosols
             #
             extinction_aerosols = k / particle_params.visibility
-            scatter_aerosols = w * extinction_aerosols * atmo_utils.calcHG(self.mu, g) * scatter_aerosols_pre
+            scatter_aerosols = w * extinction_aerosols * self.H_pol * (A_aerosols_ * atmo_utils.calcHG(self.mu, g))
             exp_aerosols = np.exp(-extinction_aerosols * exp_aerosols_pre)
             
             #
@@ -186,9 +183,8 @@ class Camera(object):
         #
         # Precalcuate the air scattering and attenuation
         #
-        scatter_air_pre = (1 + self.mu**2) * 3 / (16*np.pi) * (self.H_pol * self.A_air_)
+        scatter_air_pre = 3 / (16*np.pi) * self.H_pol * ((1 + self.mu**2) * self.A_air_)
         exp_air_pre = self.H_distances * self.A_air_
-        scatter_aerosols_pre = self.H_pol * A_aerosols_
         exp_aerosols_pre = self.H_distances * A_aerosols_
 
         gimg = []
@@ -202,26 +198,24 @@ class Camera(object):
             #
             # Calculate scattering and extiniction for aerosols
             #
+            P_mu = atmo_utils.calcHG(self.mu, g)
             extinction_aerosols = k / particle_params.visibility
-            scatter_aerosols_partial = w * extinction_aerosols * atmo_utils.calcHG(self.mu, g)
-            scatter_aerosols =  atmo_utils.spdiag(scatter_aerosols_partial * scatter_aerosols_pre)
-            scatter_aerosols_grad = self.H_pol.T * atmo_utils.spdiag(scatter_aerosols_partial)
+            scatter_aerosols =  w * extinction_aerosols * self.H_pol * (A_aerosols_ * P_mu)
             exp_aerosols = atmo_utils.spdiag(np.exp(-extinction_aerosols * exp_aerosols_pre))
-            exp_aerosols_grad = -extinction_aerosols * self.H_distances.T * exp_aerosols
             
             #
             # Calculate scattering and extiniction for air
             #
             extinction_air = 1.09e-3 * lambda_**-4.05
-            scatter_air = atmo_utils.spdiag(extinction_air * scatter_air_pre)
+            scatter_air = extinction_air * scatter_air_pre
             exp_air = atmo_utils.spdiag(np.exp(-extinction_air * exp_air_pre))
             
             #
             # Calculate the gradient of the radiance
             #
-            temp1 = scatter_aerosols_grad * exp_aerosols + exp_aerosols_grad * scatter_aerosols
-            temp2 = exp_aerosols_grad * scatter_air
-            radiance_gradient = (temp1 + temp2) * exp_air
+            temp1 =  w * extinction_aerosols * atmo_utils.spdiag(P_mu) * self.H_pol.T
+            temp2 = extinction_aerosols * self.H_distances.T * atmo_utils.spdiag(scatter_air + scatter_aerosols)
+            radiance_gradient = (temp1 - temp2) * exp_air * exp_aerosols
     
             #
             # Calculate projection on camera

simulateAtmoGeneral3D.py
@@ -95,8 +95,9 @@ def serial(particle_params):
     ATMO_aerosols = np.exp(-h/atmosphere_params.aerosols_typical_h)
     ATMO_air = np.exp(-h/atmosphere_params.air_typical_h)
 
+    results_path = amitibo.createResultFolder(params=[atmosphere_params, particle_params, camera_params])
 
-    for i, sun_angle in enumerate((0,)):#numpy.linspace(0, numpy.pi/2, 4)):
+    for i, sun_angle in enumerate(np.linspace(0, np.pi/2, 4)):
         #
         # Instantiating the camera
         #
@@ -107,12 +108,13 @@ def serial(particle_params):
             camera_position=(width/2, width/2, 0.2)
         )
         
+        cam.setA_air(ATMO_air)
+        
         #
         # Calculating the image
         #
-        img = cam.calcImage(A_air=ATMO_air, A_aerosols=ATMO_aerosols, particle_params=particle_params)
+        img = cam.calcImage(A_aerosols=ATMO_aerosols, particle_params=particle_params)
         
-        results_path = amitibo.createResultFolder(params=[atmosphere_params, particle_params, camera_params])
         sio.savemat(os.path.join(results_path, 'img%d.mat' % i), {'img':img}, do_compression=True)
         
 
@@ -135,9 +137,11 @@ if __name__ == '__main__':
         visibility=10
         )
 
-    if profile:
-        import cProfile    
-        cmd = "serial(particle_params)"
-        cProfile.runctx(cmd, globals(), locals(), filename="atmosphere_camera.profile")
-    else:
-        parallel(particle_params)
+    #if profile:
+        #import cProfile    
+        #cmd = "serial(particle_params)"
+        #cProfile.runctx(cmd, globals(), locals(), filename="atmosphere_camera.profile")
+    #else:
+        #parallel(particle_params)
+        
+    serial(particle_params)