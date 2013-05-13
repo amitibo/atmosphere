Some bug fix to the analysis (removed the derivative multiplication to the scattering). Added test to the camera. Currently the error is not high but ecceeds the estimated error.

atmotomo/camera.py
@@ -150,7 +150,7 @@ class Camera(object):
         """Precalculate the air extinction and scattering"""
         
         self._air_exts = [np.exp(-self.H_distances * air_ext_coef) for air_ext_coef in air_ext_coefs]
-        self._air_scat = [(3 / (16*np.pi) * (1 + self.mu**2) * (self.H_cart2polar * air_ext_coef)*self.R_derivatives) for air_ext_coef in air_ext_coefs]
+        self._air_scat = [(3 / (16*np.pi) * (1 + self.mu**2) * (self.H_cart2polar * air_ext_coef)) for air_ext_coef in air_ext_coefs]
         
     def calcImage(self, A_aerosols, particle_params, add_noise=False):
         """Calculate the image for a given aerosols distribution"""
@@ -174,7 +174,7 @@ class Camera(object):
             #
             # Calculate scattering and extiniction for aerosols
             #
-            scatter_aerosols = w * k * ((self.H_cart2polar * A_aerosols_) * atmo_utils.calcHG(self.mu, g) * self.R_derivatives)
+            scatter_aerosols = w * k * (self.H_cart2polar * A_aerosols_) * atmo_utils.calcHG(self.mu, g)
             exp_aerosols = np.exp(-k * exp_aerosols_pre)
             
             #

scripts/analyzeAtmo3D.py
@@ -5,7 +5,7 @@ from __future__ import division
 import numpy as np
 from atmotomo import calcHG, L_SUN_RGB, RGB_WAVELENGTH, getResourcePath
 from atmotomo import Camera
-from atmotomo import density_clouds1, calcAirMcarats, getMisrDB, Mcarats
+from atmotomo import density_clouds1, calcAirMcarats, getMisrDB, Mcarats, single_voxel_atmosphere
 import sparse_transforms as spt
 import atmotomo
 import scipy.io as sio
@@ -36,6 +36,8 @@ IMGTAG = 1
 OBJTAG = 2
 GRADTAG = 3
 DIETAG = 4
+
+VISIBILITY = 10000
 
 MAX_ITERATIONS = 4000
 KM_TO_METER = 1000
@@ -84,8 +86,9 @@ SUN_ANGLE = -np.pi/4
 REF_IMG_SCALE = 10.0**4
 MCARATS_IMG_SCALE = 10.0**9.7
 
-VADIM_IMG_SCALE = 9.75122
-
+#VADIM_IMG_SCALE = 9.75122
+#VADIM_IMG_SCALE = 2493.67
+VADIM_IMG_SCALE = 880
 profile = False
 
 
@@ -221,7 +224,9 @@ def master(particle_params, solver='ipopt'):
     #
     # Create the distributions
     #
-    A_air, A_aerosols, Y, X, H, h = density_clouds1(atmosphere_params)
+    #A_air, A_aerosols, Y, X, H, h = density_clouds1(atmosphere_params)
+    A_aerosols, Y, X, H = single_voxel_atmosphere(atmosphere_params, indices_list=[(24, 24, 19)], density=1/VISIBILITY, decay=False)
+    A_aerosols = A_aerosols[0]
     
     z_coords = H[0, 0, :]
     air_exts = calcAirMcarats(z_coords)
@@ -403,7 +408,7 @@ def slave(particle_params, camera_position, ref_img, scaling=1, no_air=False):
     )
 
 
-def loadSlaveData(ref_images, mcarats, sigma):
+def loadSlaveData(ref_images, mcarats, simulate, sigma):
     """"""
     
     global mpi_size
@@ -449,7 +454,10 @@ def loadSlaveData(ref_images, mcarats, sigma):
         
         if mpi_rank >= mpi_size:
             sys.exit()
-            
+        
+        #
+        # Load the reference images
+        #
         if mpi_rank > 0:
             path = folder_list[mpi_rank-1]
             img_path = os.path.join(path, "RGB_MATRIX.mat")
@@ -483,10 +491,13 @@ def loadSlaveData(ref_images, mcarats, sigma):
         if mpi_rank > 0:
             camera_position = CAMERA_CENTERS[mpi_rank-1]
 
+    if simulate:
+        ref_img = None
+        
     return ref_img, camera_position, scaling
     
 
-def main(ref_images=None, mcarats=None, sigma=0.0, no_air=False):
+def main(ref_images=None, mcarats=None, simulate=False, sigma=0.0, no_air=False):
     
     global mpi_size
     
@@ -509,7 +520,7 @@ def main(ref_images=None, mcarats=None, sigma=0.0, no_air=False):
     # but it also calculated the mpi_size which important for the master
     # also)
     #
-    ref_img, camera_position, scaling = loadSlaveData(ref_images, mcarats, sigma)
+    ref_img, camera_position, scaling = loadSlaveData(ref_images, mcarats, simulate, sigma)
     
     if mpi_rank == 0:
         #
@@ -529,6 +540,7 @@ if __name__ == '__main__':
     parser.add_argument('--ref_images', help='path to reference images')
     parser.add_argument('--sigma', type=float, default=0.0, help='smooth the reference image by sigma')
     parser.add_argument('--no_air', action='store_true', help='Use atmosphere without air molecules')
+    parser.add_argument('--simulate', action='store_true', help='Use simulated images (overrides previous flags like mcarats and ref_images).')
     args = parser.parse_args()
 
-    main(args.ref_images, args.mcarats, args.sigma, args.no_air)
+    main(args.ref_images, args.mcarats, args.simulate, args.sigma, args.no_air)

scripts/pbsMcarats.py
@@ -31,10 +31,10 @@ def prepareSimulationFiles(results_path, cameras_file, img_size, target):
     # Simulation parameters
     #
     atmosphere_params = amitibo.attrClass(
-        cartesian_grids=(
-            slice(0, ATMOSPHERE_WIDTH, 1000.0), # Y
-            slice(0, ATMOSPHERE_WIDTH, 1000.0), # X
-            slice(0, ATMOSPHERE_HEIGHT, 100.)   # H
+        cartesian_grids=spt.Grids(
+            np.arange(0, 50000, 1000.0), # Y
+            np.arange(0, 50000, 1000.0), # X
+            np.arange(0, 10000, 100.0)   # H
             ),
         earth_radius=4000000,
         RGB_WAVELENGTH=RGB_WAVELENGTH,

scripts/simulateAtmo3D.py
@@ -52,7 +52,7 @@ KM_TO_METER = 1000
 profile = False
     
 
-def parallel(particle_params, cameras):
+def parallel(particle_params, cameras, add_noise):
     
     from mpi4py import MPI
     
@@ -74,6 +74,8 @@ def parallel(particle_params, cameras):
     #A_air, A_aerosols, Y, X, H, h = density_clouds1(atmosphere_params)
     #A_aerosols = A_aerosols / VISIBILITY
     A_aerosols, Y, X, Z = single_voxel_atmosphere(atmosphere_params, indices_list=[(24, 24, 19)], density=1/VISIBILITY, decay=False)
+    A_aerosols = A_aerosols[0]
+    #A_aerosols = sio.loadmat('/u/amitibo/code/atmosphere/results/210+/13_05_08__09_01_43/radiance.mat')['estimated']
     
     #
     # Instantiating the camera
@@ -90,12 +92,12 @@ def parallel(particle_params, cameras):
         #z_coords = H[0, 0, :]
         #air_exts = calcAirMcarats(z_coords)
         #cam.set_air_extinction(air_exts)
-        cam.setA_air(np.zeros_like(A_aerosols[0]))
+        cam.setA_air(np.zeros_like(A_aerosols))
         
         #
         # Calculating the image
         #
-        img = cam.calcImage(A_aerosols=A_aerosols[0], particle_params=particle_params, add_noise=True)
+        img = cam.calcImage(A_aerosols=A_aerosols, particle_params=particle_params, add_noise=add_noise)
     else:
         img = []
         
@@ -108,7 +110,7 @@ def parallel(particle_params, cameras):
                 sio.savemat(os.path.join(results_path, 'img%d.mat' % i), {'img':img}, do_compression=True)
     
     
-def serial(particle_params):
+def serial(particle_params, add_noise):
     
     results_path = amitibo.createResultFolder(params=[atmosphere_params, particle_params, camera_params], src_path=atmotomo.__src_path__)
    
@@ -136,7 +138,7 @@ def serial(particle_params):
         #
         # Calculating the image
         #
-        img = cam.calcImage(A_aerosols=A_aerosols, particle_params=particle_params, add_noise=True)
+        img = cam.calcImage(A_aerosols=A_aerosols, particle_params=particle_params, add_noise=add_noise)
         
         sio.savemat(os.path.join(results_path, 'img%d.mat' % i), {'img':img}, do_compression=True)
         
@@ -151,6 +153,7 @@ if __name__ == '__main__':
     parser.add_argument('--ref_images', help='path to reference images')
     parser.add_argument('--parallel', action='store_true', help='run the parallel mode')
     parser.add_argument('--profile', action='store_true', help='run the profiler (will use serial mode)')
+    parser.add_argument('--noise', action='store_true', help='Add noise to the image creation')
     args = parser.parse_args()
     
     cameras = []
@@ -196,10 +199,10 @@ if __name__ == '__main__':
 
     if args.profile:
         import cProfile    
-        cmd = "serial(particle_params)"
+        cmd = "serial(particle_params, args.noise)"
         cProfile.runctx(cmd, globals(), locals(), filename="atmosphere_camera.profile")
     else:
         if args.parallel:
-            parallel(particle_params, cameras)
+            parallel(particle_params, cameras, args.noise)
         else:
-            serial(particle_params)
+            serial(particle_params, args.noise, args.noise)