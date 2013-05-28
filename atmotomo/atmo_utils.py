REMEMBER: Next time the simulation produces bad images, check the units. I spent all night to find that the bug was that I forgot to fix the units of the air scattering formula when I moved from KM to Meters (length units):  1.09e-3 -> 1.09e-6. Small modifications to the configuration files. Added randomness (with constant seed) to the cameras locations in the configuration files.

atmotomo/atmo_utils.py
@@ -92,7 +92,7 @@ def calcHG_other(mu, g):
     return HG
 
 
-def calcScatterMu(grids, sun_angle):
+def calcScatterMu(grids, sun_angle_phi, sun_angle_theta):
     """
     Calclculate the cosine of the scattering angle at each voxel.
     """
@@ -100,7 +100,7 @@ def calcScatterMu(grids, sun_angle):
     #
     # Rotate the grids so that the Z axis will point in the direction of the sun
     #
-    grids_rotated = grids.rotate(-sun_angle[1], sun_angle[0], 0)
+    grids_rotated = grids.rotate(-sun_angle_theta, sun_angle_phi, 0)
     
     Z_rotated = grids_rotated[2]
     R_rotated = np.sqrt(grids_rotated[0]**2 + grids_rotated[1]**2 + grids_rotated[2]**2)
@@ -146,7 +146,7 @@ def loadVadimData(path, offset=(0, 0), remove_sunspot=False, FACMIN=20):
     
     img_list = []
     cameras_list = []
-    for folder in folder_list:
+    for i, folder in enumerate(folder_list):
         #
         # Load the image data
         #
@@ -161,8 +161,8 @@ def loadVadimData(path, offset=(0, 0), remove_sunspot=False, FACMIN=20):
 
         if remove_sunspot:
             R, G, B = img[:, :, 0], img[:, :, 1], img[:, :, 2]
-            Rmax = Mcarats.calcRmax(R, FACMIN=FACMIN)
-            ys, xs = np.nonzero(R>Rmax)
+            Rmax = R.max()#Mcarats.calcRmax(R, FACMIN=FACMIN)
+            ys, xs = np.nonzero(R>(Rmax*0.9))
             R, G, B = [Mcarats.removeSunSpot(ch, ys, xs, MARGIN=1) for ch in (R, G, B)]
             img = np.dstack((R, G, B))
         
@@ -189,6 +189,12 @@ def readConfiguration(path):
     import sparse_transforms as spt
     import scipy.io as sio
     
+    if os.path.isdir(path):
+        base_path = path
+        path = os.path.join(path, 'configuration.ini')
+    else:
+        base_path, dump = os.path.split(path)
+        
     config = ConfigObj(path)
     
     #
@@ -215,7 +221,8 @@ def readConfiguration(path):
     sun_section = config['sun']
 
     sun_params = amitibo.attrClass(
-        angle=[float(i) for i in sun_section.as_list('angle')],
+        angle_phi=sun_section.as_float('angle_phi'),
+        angle_theta=sun_section.as_float('angle_theta'),
         intensities=[float(i) for i in sun_section['intensities']],
         wavelengths=[float(i) for i in sun_section['wavelengths']]
     )
@@ -225,8 +232,8 @@ def readConfiguration(path):
     #
     try:
         distributions_section = config['distributions']
-        air_dist_path = os.path.join(os.path.abspath(distributions_section['air_dist_path']))
-        aerosols_dist_path = os.path.join(os.path.abspath(distributions_section['aerosols_dist_path']))
+        air_dist_path = os.path.join(base_path, distributions_section['air_dist_path'])
+        aerosols_dist_path = os.path.join(base_path, distributions_section['aerosols_dist_path'])
     except KeyError, e:
         air_dist_path = os.path.join(os.path.abspath('.'))
         aerosols_dist_path = os.path.join(os.path.abspath('.'))
@@ -261,7 +268,8 @@ def readConfiguration(path):
     camera_z =  np.array([float(i) for i in camera_section.as_list('z')])
 
     camera_params = amitibo.attrClass(
-        resolution=[int(i) for i in camera_section['resolution']]
+        resolution=[int(i) for i in camera_section['resolution']],
+        type=camera_section['type']
         )
     
     cameras=[(camera_y[i], camera_x[i], camera_z[i]) for i in range(camera_section.as_int('cameras_num'))]

atmotomo/densities.py
@@ -4,7 +4,6 @@
 from __future__ import division
 import numpy as np
 from atmotomo import L_SUN_RGB, RGB_WAVELENGTH
-import sparse_transforms as spt
 import amitibo
 import os
 
@@ -19,17 +18,6 @@ __all__ = [
 ]
 
 SIMULATION_TEMPLATE_FILE_NAME = 'simulation.jinja'
-
-clouds_atmosphere = amitibo.attrClass(
-    cartesian_grids=spt.Grids(
-        np.arange(0, 50000, 1000.0), # Y
-        np.arange(0, 50000, 1000.0), # X
-        np.arange(0, 10000, 100.0)   # H
-        ),
-    earth_radius=4000000,
-    air_typical_h=8000,
-    aerosols_typical_h=2000
-)
 
 
 def density_front(atmosphere_params):
@@ -59,12 +47,13 @@ def density_front(atmosphere_params):
 
 
 def clouds_simulation(
-    atmosphere_params=clouds_atmosphere,
+    atmosphere_params,
     particle_name='spherical_nonabsorbing_2.80',
     particle_phase='HG',
     camera_resolution=(128, 128),
     camera_type='linear',
-    sun_angle=(0, -np.pi/4),
+    sun_angle_phi=0,
+    sun_angle_theta=-np.pi/4,
     visibility=100000
     ):
 
@@ -97,14 +86,27 @@ def clouds_simulation(
     #
     # Create the cameras
     #
+    dy = derivs[0].ravel()[0]
+    dx = derivs[1].ravel()[0]
+    dz = derivs[2].ravel()[0]
     camera_X, camera_Y = np.meshgrid(
         np.linspace(0, width, 12)[1:-1],
         np.linspace(0, width, 12)[1:-1]
         )
+    camera_Y = camera_Y.ravel()
+    camera_X = camera_X.ravel()
     
+    np.random.seed(0)
+    camera_Y += (2*np.random.rand(camera_X.size)-1) * 10
+    camera_X += (2*np.random.rand(camera_X.size)-1) * 10
+    camera_Z = np.random.rand(camera_X.size) * 10
+    
+    #
+    # Store the data
+    #
     data = {
-        'dy':derivs[0].ravel()[0],
-        'dx':derivs[1].ravel()[0],
+        'dy':dy,
+        'dx':dx,
         'ny':Y.shape[0],
         'nx':Y.shape[1],
         'nz':Y.shape[2],
@@ -114,10 +116,11 @@ def clouds_simulation(
         'camera_resolution':camera_resolution,
         'camera_type':camera_type,
         'cameras_num':100,
-        'camera_ypos':camera_Y.ravel(),
-        'camera_xpos':camera_X.ravel(),
-        'camera_zpos':np.ones(camera_X.size),
-        'sun_angle':sun_angle,
+        'camera_ypos':camera_Y,
+        'camera_xpos':camera_X,
+        'camera_zpos':camera_Z,
+        'sun_angle_phi':sun_angle_phi,
+        'sun_angle_theta':sun_angle_theta,
         'sun_intensities':L_SUN_RGB,
         'sun_wavelengths':RGB_WAVELENGTH
     }
@@ -239,6 +242,15 @@ def prepareSimulation(path, func, *params, **kwrds):
     BaseData._tpl_env = tpl_env
     simulation = BaseData(template_path=SIMULATION_TEMPLATE_FILE_NAME)
     
+    #
+    # Create the path if necessary
+    #
+    if not os.path.isdir(path):
+        os.makedirs(path)
+        
+    #
+    # Create the simulation data using the func function.
+    #
     data, air_dist, aerosols_dist = func(*params, **kwrds)
     
     sio.savemat(
@@ -246,14 +258,14 @@ def prepareSimulation(path, func, *params, **kwrds):
         {'distribution': air_dist},
         do_compression=True
     )
-    simulation.addData(air_dist_path=os.path.join(path, 'air_dist.mat'))
+    simulation.addData(air_dist_path='./air_dist.mat')
     
     sio.savemat(
         os.path.join(path, 'aerosols_dist.mat'),
         {'distribution': aerosols_dist},
         do_compression=True
     )
-    simulation.addData(aerosols_dist_path=os.path.join(path, 'aerosols_dist.mat'))
+    simulation.addData(aerosols_dist_path='./aerosols_dist.mat')
     
     simulation.addData(**data)
     

scripts/calcRatio.py
@@ -5,6 +5,7 @@ Calculate the ratio between two sets of images.
 from __future__ import division
 import numpy as np
 import matplotlib.pyplot as plt
+import atmotomo
 import argparse
 import glob
 import scipy.io as sio
@@ -14,21 +15,8 @@ import skimage.morphology as morph
 
 
 def getMcMatList(path):
-    
-    folder_list = glob.glob(os.path.join(path, "*"))
-    if not folder_list:
-        raise Exception("No img found in the folder")
-    
-    img_list = []
-    for folder in folder_list:
-        print folder
-        img_path = os.path.join(folder, "RGB_MATRIX.mat")
-        try:
-            data = sio.loadmat(img_path)
-        except:
-            continue
-        
-        img_list.append(data['Detector'])
+
+    img_list, cameras = atmotomo.loadVadimData(path, remove_sunspot=False)
     
     return img_list
 
@@ -38,9 +26,11 @@ def getSingleMatList(path):
     path = os.path.abspath(path)
     files_list = glob.glob(os.path.join(path, "*.mat"))
     
+    file_pattern = re.search(r'(.*?)\d+.mat', files_list[0]).groups()[0]
+    
     img_list = []
-    for img_path in files_list:
-        print img_path
+    for i in range(0, len(files_list)+1):
+        img_path = os.path.join(path, "%s%d.mat" % (file_pattern, i))
         try:
             data = sio.loadmat(img_path)
         except:
@@ -54,8 +44,7 @@ def getSingleMatList(path):
 def findMatched(ref_img, single_imgs):
 
     mask_results = []
-    masks = []
-    
+
     if (ref_img>0).sum() > (ref_img.size*3/4):
         #
         # The mcarats ref images have values all over the sky
@@ -76,16 +65,34 @@ def findMatched(ref_img, single_imgs):
         
         temp = single_img[mask]
         mask_results.append(temp.sum())
-        masks.append(mask)
     
     ind = np.argmax(mask_results)
+        
     print ind
-    return single_imgs[ind], masks[ind]
+    return single_imgs[ind]
+
+
+def calcRatio(ref_img, single_img, erode):
+    #
+    # Calc a joint mask
+    #
+    mask = (ref_img > 0) * (single_img > 0)
+    if erode:
+        for i in range(3):
+            mask[:, :, i] = morph.greyscale_erode(mask[:, :, i].astype(np.uint8) , morph.disk(1))
+        mask = mask>0
+    
+    ratio = ref_img[mask].mean() / single_img[mask].mean()
+
+    return ratio
 
 
 def main(ref_path, single_path, vadim_ref):
     """Main doc """
     
+    #
+    # Load the reference images and single scattering images
+    #
     if vadim_ref:
         ref_imgs = getMcMatList(ref_path)
     else:
@@ -93,40 +100,44 @@ def main(ref_path, single_path, vadim_ref):
         
     single_imgs = getSingleMatList(single_path)
 
-    matched_single_imgs = []
-    matched_masks = []
-    for ref_img in ref_imgs:
-        single_img, mask = findMatched(ref_img, single_imgs)
-        matched_single_imgs.append(single_img)
-        matched_masks.append(mask)
-        
+    #
+    # Match the images.
+    #
+    #matched_single_imgs = []
+    #for ref_img in ref_imgs:
+        #single_img = findMatched(ref_img, single_imgs)
+        #matched_single_imgs.append(single_img)
+    if vadim_ref:
+        matched_single_imgs = single_imgs[:]
+    else:
+        matched_single_imgs = single_imgs[:]
+    
     means = []
-    for ref_img, single_img, mask in zip(ref_imgs, matched_single_imgs, matched_masks):
-        ratio = ref_img[mask].mean() / single_img[mask].mean()
-        
-        means.append(ratio)
-        
-        showImages(ref_img, single_img, mask, ratio)
+    for ref_img, single_img in zip(ref_imgs, matched_single_imgs):
+        means.append(calcRatio(ref_img, single_img, not vadim_ref))
+        showImages(ref_img, single_img)
         
     print 'Mean: %g (=10^%g)' % (np.mean(means[7:]), np.log10(np.mean(means[7:])))
 
     fig = plt.figure()
     ax = fig.add_subplot(111)
     rects1 = ax.bar(np.arange(len(means)), means, color='r')
+    if vadim_ref:
+        plt.title('Ratio Between Single Scattering and Vadim MC (isotropic phase)')
+    else:
+        plt.title('Ratio Between Single Scattering and MCARATS')
+        
     plt.show()
     
 
-def showImages(ref_img, single_img, mask, ratio):
+def showImages(ref_img, single_img):
     plt.figure()
-    plt.subplot(131)
+    plt.subplot(121)
     plt.imshow(ref_img/ref_img.max())
     plt.title('Reference')
-    plt.subplot(132)
+    plt.subplot(122)
     plt.imshow(single_img/single_img.max())
     plt.title('Single')
-    plt.subplot(133)
-    plt.imshow(mask)
-    plt.title('Mask')
 
 
 if __name__ == '__main__':

scripts/createConfigurations.py
@@ -2,12 +2,36 @@
 Create configuration files for a atmosphere simulations.
 """
 from __future__ import division
+import sparse_transforms as spt
 import numpy as np
 import atmotomo 
 import scipy.io as sio
 import os
 import argparse
+import amitibo
 
+
+clouds_atmosphere = amitibo.attrClass(
+    cartesian_grids=spt.Grids(
+        np.arange(0, 50000, 1000.0), # Y
+        np.arange(0, 50000, 1000.0), # X
+        np.arange(0, 10000, 100.0)   # H
+        ),
+    earth_radius=4000000,
+    air_typical_h=8000,
+    aerosols_typical_h=2000
+)
+
+clouds_atmosphere_low_resolution = amitibo.attrClass(
+    cartesian_grids=spt.Grids(
+        np.arange(0, 50000, 10000.0), # Y
+        np.arange(0, 50000, 10000.0), # X
+        np.arange(0, 10000, 1000.0)   # H
+        ),
+    earth_radius=4000000,
+    air_typical_h=8000,
+    aerosols_typical_h=2000
+)
 
 def main(output_path):
     #
@@ -16,16 +40,37 @@ def main(output_path):
     if not os.path.isdir(output_path):
         os.makedirs(output_path)
 
-    atmotomo.prepareSimulation(path=output_path, func=atmotomo.clouds_simulation)
-    
+    atmotomo.prepareSimulation(
+        path=os.path.join(output_path, 'two_clouds_low_density'),
+        func=atmotomo.clouds_simulation,
+        atmosphere_params=clouds_atmosphere,
+        visibility=100000
+    )
+    atmotomo.prepareSimulation(
+        path=os.path.join(output_path, 'two_clouds_high_density'),
+        func=atmotomo.clouds_simulation,
+        atmosphere_params=clouds_atmosphere,
+        visibility=10000
+    )
+    atmotomo.prepareSimulation(
+        path=os.path.join(output_path, 'two_clouds_high_density_low_resolution'),
+        func=atmotomo.clouds_simulation,
+        atmosphere_params=clouds_atmosphere_low_resolution,
+        visibility=10000
+    )
+
+
 if __name__ == '__main__':
     #
     # Parse the input
     #
     parser = argparse.ArgumentParser(description='Create configuration files for the atmosphere simulations')
-    parser.add_argument('output_path', help='path to configuration files')
+    parser.add_argument('--output_path', default=None, help='path to configuration files')
     args = parser.parse_args()
     
-    output_path = os.path.abspath(args.output_path)
+    if args.output_path == None:
+        output_path = amitibo.getResourcePath('configurations', package_name='atmotomo')
+    else:
+        output_path = os.path.abspath(args.output_path)
 
     main(output_path)

scripts/resultsAtmoDualGui.py
@@ -21,6 +21,7 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 import scipy.io as sio
 import scipy.ndimage as ndimage
+from atmotomo import loadVadimData
 import numpy as np
 import amitibo
 import glob
@@ -294,21 +295,7 @@ class resultAnalayzer(HasTraits):
         path = self.tr_DND_vadim[0].absolute_path
          
         path, folder_name =  os.path.split(path)
-        folder_list = glob.glob(os.path.join(path, "*"))
-        if not folder_list:
-            warning(info.ui.control, "No img found in the folder", "Warning")
-            return
-        
-        self._images_vadim = []
-        for folder in folder_list:
-            img_path = os.path.join(folder, "RGB_MATRIX.mat")
-            try:
-                data = sio.loadmat(img_path)
-            except:
-                continue
-            
-            self._images_vadim.append(data['Detector'])
-
+        self._images_vadim, dumb = loadVadimData(path)        
         self.tr_vadim_len = len(self._images_vadim) - 1
         
         self._updateImg()

scripts/viz3Dradiance.py
@@ -8,26 +8,27 @@ import scipy.io as sio
 import mayavi.mlab as mlab
 import matplotlib.pyplot as plt
 from atmotomo import single_cloud_vadim
+import sparse_transforms as spt
 import argparse
 import amitibo
 
 
 atmosphere_params = amitibo.attrClass(
-    cartesian_grids=(
-        slice(0, 50, 1.0), # Y
-        slice(0, 50, 1.0), # X
-        slice(0, 10, 0.1)   # H
+    cartesian_grids=spt.Grids(
+        np.arange(0, 50000, 1000.0), # Y
+        np.arange(0, 50000, 1000.0), # X
+        np.arange(0, 10000, 100.0)   # H
         ),
-    earth_radius=4000,
-    air_typical_h=8,
-    aerosols_typical_h=2
+    earth_radius=4000000,
+    air_typical_h=8000,
+    aerosols_typical_h=2000
 )
 
 def main():
     """Main doc """
 
     parser = argparse.ArgumentParser(description='Visualize a 3D matlab matrix')
-    parser.add_argument('path', help='Path to matlab matrix')
+    parser.add_argument('path', help='Path to radiance matrix')
     
     args = parser.parse_args()
     
@@ -42,20 +43,11 @@ def main():
     # This comes from out use of A:
     # exp(-A / visibility * length) = exp(-k * N * length)
     #
-    ratio = 1 / 50 / 0.00072
-    radiance_estim = data['estimated'] * ratio
+    #ratio = 1 / 10000 / 0.00072
+    radiance_estim = data['estimated'] 
+    radiance_true = data['true']
     
-    #
-    # Remove negative values from the estimation
-    #
-    radiance_estim[:3, :, :] = 0
-    radiance_estim[:, :3, :] = 0    
-    
-    #
-    # Create the distributions
-    #
-    A_air, radiance_true, Y, X, H, h = single_cloud_vadim(atmosphere_params)
-    radiance_true = radiance_true[::-1, :, :] * ratio
+    Y, X, H = atmosphere_params.cartesian_grids.expanded
     
     amitibo.viz3D(Y, X, H, radiance_true, title='Monte Carlo')
     amitibo.viz3D(Y, X, H, radiance_estim, title='Estimated',)
@@ -66,6 +58,18 @@ def main():
     print 'Absolute error normalized to num of particles:', error_per_voxel
     
     mlab.show()
+    
+    #plt.figure()
+    #plt.imshow(radiance_true[:, 24, ::-1].T, interpolation='nearest', cmap='gray')
+    #plt.colorbar()
+    #plt.title('True Density')
+    
+    #plt.figure()
+    #plt.imshow(radiance_estim[:, 24, ::-1].T, interpolation='nearest', cmap='gray')
+    #plt.colorbar()
+    #plt.title('Estimated Density')
+    
+    #plt.show()
     
     #plt.figure(figsize=(8, 2))
     #plt.plot(np.log(data['objective']))

tests/atmosphere.py
@@ -13,7 +13,7 @@ class Test(unittest.TestCase):
         
     def testHG(self):
         
-        g = 0.4
+        g = -1.0
         angle = np.linspace(0, np.pi, 200)
         hg1 = calcHG(np.cos(angle), g)
         hg2 = calcHG_other(np.cos(angle), g)
@@ -23,6 +23,7 @@ class Test(unittest.TestCase):
         plt.figure()
         plt.plot(angle, hg1)
         plt.plot(angle, hg2)
+        plt.legend(('standard', 'double'))
         plt.show()
     
 