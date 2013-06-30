Bug fix (?) Seems that when using loadmat, the array uses Fortran order. This might causes transpose error in the gradient and other functions. Seems to work on small problems.

atmotomo/atmo_utils.py
@@ -152,7 +152,7 @@ def loadVadimData(path, offset=(0, 0), remove_sunspot=False, FACMIN=20):
         #
         img_path = os.path.join(folder, "RGB_MATRIX.mat")
         try:
-            data = sio.loadmat(img_path)
+            data = np.array(sio.loadmat(img_path), copy=True, order='C')
         except:
             print 'No image data in folder:', folder
             continue
@@ -238,8 +238,8 @@ def readConfiguration(path):
         air_dist_path = os.path.join(os.path.abspath('.'))
         aerosols_dist_path = os.path.join(os.path.abspath('.'))
         
-    air_dist = sio.loadmat(air_dist_path)['distribution']
-    aerosols_dist = sio.loadmat(aerosols_dist_path)['distribution']
+    air_dist = np.array(sio.loadmat(air_dist_path)['distribution'], copy=True, order='C')
+    aerosols_dist = np.array(sio.loadmat(aerosols_dist_path)['distribution'], copy=True, order='C')
     
     #
     # Load particle

scripts/analyzeAtmo3D.py
@@ -140,9 +140,9 @@ class RadianceProblem(object):
             comm.Recv(temp, source=MPI.ANY_SOURCE, status=sts)
             grad += temp
         
-        grad_numerical = approx_fprime(x.ravel(), self.objective, epsilon=1e-8)
-        np.save('grad.npy', grad)
-        np.save('grad_numerical.npy', grad_numerical)
+        #grad_numerical = approx_fprime(x.ravel(), self.objective, epsilon=1e-8)
+        #np.save('grad.npy', grad)
+        #np.save('grad_numerical.npy', grad_numerical)
         
         #
         # For some reason the gradient is transposed. I found it out by comparing
@@ -150,7 +150,7 @@ class RadianceProblem(object):
         # Fortran and C order representation possibly due to loading of the configuration
         # files.
         #
-        grad = np.transpose(grad.reshape(self._atmo_shape))
+        #grad = np.transpose(grad.reshape(self._atmo_shape))
         return grad.flatten()
 
     def intermediate(
@@ -185,7 +185,7 @@ class RadianceProblem(object):
 def master(air_dist, aerosols_dist, results_path, solver='ipopt', job_id=None):
     #import rpdb2; rpdb2.start_embedded_debugger('pep')
     
-    import wingdbstub
+    #import wingdbstub
 
     logging.basicConfig(
         filename=os.path.join(results_path, 'run.log'),

scripts/createConfigurations.py
@@ -25,8 +25,8 @@ clouds_atmosphere = amitibo.attrClass(
 clouds_atmosphere_low_resolution = amitibo.attrClass(
     cartesian_grids=spt.Grids(
         np.arange(0, 50, 10.0), # Y
-        np.arange(0, 50, 8.0), # X
-        np.arange(0, 10, 3.0)   # H
+        np.arange(0, 50, 10.0), # X
+        np.arange(0, 10, 2.0)   # H
         ),
     earth_radius=4000,
     air_typical_h=8,