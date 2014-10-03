f36bd38fdf75d9ed6f5d1d94991660741beb2277
amitibo <amitibo@localhost>
2014-10-03 09:02:01 +0300

    bookmark (broken)

--- atmotomo/shdom.py
+++ atmotomo/shdom.py
@@ -3,7 +3,7 @@
 
 from __future__ import division
 import numpy as np
-
+import subprocess as sbp
 
 __all__ = (
     'calcTemperature',
@@ -69,7 +69,90 @@ def createMassContentFile(
         for i, j, k, m in zip(i_ind, j_ind, k_ind, mass_content.ravel()):
             f.write('%d\t%d\t%d\t1\t1\t%.5f\t%.5f\n' % (i+1, j+1, k+1, m, char_radius))
 
+
+def createParticleMixtureFile(
+    file_path,
+    atmosphere_params,
+    char_radius,
+    mass_content=None,
+    particle_dist=None,
+    cross_section=None
+    ):
+    """
+    This function creates a new .part file 
+    If mass_content is given it is used as the extinction matrix. If not
+    the particle properties are taken from the MISR table and the given distribution.
+    """
     
-if __name__ == '__main__':
+    wavenames = ('red', 'green', 'blue')
+    wavelengths = (0.672, 0.558, 0.446)
+    rayl_coef_array = (0.00148, 0.0031, 0.0079)
+
+    # Wavelength dependent rayleigh coefficient at sea level 
+    # calculated by k=(2.97e-4)*lambda^(-4.15+0.2*lambda)
+    
+    # parfile = ${basefile}.part
+    maxnewphase = 50
+    asymtol = 0.1
+    fracphasetol = 0.1
+    Nzother = 0
+    
+    for wavename, wavelen, raylcoef in ():
+        prpfile = """${basefile}_${wavename}.prp"""
+        scattable = """("${basefile}_${wavename}_Mie.scat"""        
+
+        
+        put $#scattable $scattable "$scattypes" $parfile \
+            $maxnewphase $asymtol $fracphasetol $raylcoef \
+            $Nzother $prpfile  | propgen   
+
+        p = sbp.Popen(['propgen'], stdout=sbp.PIPE, stdin=sbp.PIPE, stderr=sbp.STDOUT)'
+        p.communicate(
+            input="1 "
+        )
+
+
+def createScatFile(base_path):
+    """
+    """
+    
+    wavenames = ('red', 'green', 'blue')
+    wavelengths = (0.672, 0.558, 0.446)
+    rayl_coef_array = (0.00148, 0.0031, 0.0079)
+
+    # Wavelength dependent rayleigh coefficient at sea level 
+    # calculated by k=(2.97e-4)*lambda^(-4.15+0.2*lambda)
+    
+    partype = "A"                # W for water
+    refindex="(1.45,-0.0006)"    # aerosol complex index of refraction 
+    pardens=1.91                 # particle bulk denisty (g/cm^3)
+    distflag=L                   # G=gamma, L=lognormal size distribution
+    sigma = 0.7                  # lognormal dist shape parameter
+    Nretab=1                     # number of effective radius in table
+    Sretab=0.57; Eretab=0.57     # starting, ending effective radius (micron)
+    maxradius=50 
+    
+    for wavename, wavelen, raylcoef in zip(wavenames, wavelengths, rayl_coef_array):
+        outfile = "{base_file}_{wavename}_Mie.scat".format(base_file=base_path, wavename=wavename)
+        p = sbp.Popen(['make_mie_table'], stdout=sbp.PIPE, stdin=sbp.PIPE, stderr=sbp.STDOUT)'
+        p.communicate(
+            input="{wavelen1} {wavelen2} {partype} {rindex} {pardens} {distflag} {alpha} {nretab} {sretab} {eretab} {maxradius} {miefile}".format(
+                wavelen1=wavelen,
+                wavelen2=wavelen,
+                partype=partype,
+                rindex=refindex,
+                pardens=pardens,
+                distflag=distflag,
+                alpha=sigma,
+                nretab=Nretab,
+                sretab=Sretab,
+                eretab=Eretab,
+                maxradius=maxradius,
+                miefile=outfile
+            )
+        )
+
+
+        if __name__ == '__main__':
     pass
     
